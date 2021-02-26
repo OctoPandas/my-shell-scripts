343ba69d0643b81910848472dc89c6984f274708
OctoPandas <63722857+OctoPandas@users.noreply.github.com>
2021-02-26 19:22:07 +0800

    Update my_custom_commands.sh

--- my_custom_commands.sh
+++ my_custom_commands.sh
@@ -3,6 +3,7 @@
 # In `~/.bashrc` or `~/.zshrc`:
 # source my_custom_commands.sh
 
+<< END_OF_FUNCTION
 ni() {
   for param do
     dir="$(dirname -- "$param")"
@@ -10,6 +11,18 @@ ni() {
       touch -- "$param"
   done
 }
+END_OF_FUNCTION
 # ni /path/file_a /path/file_b
 # from: https://unix.stackexchange.com/questions/168580/make-parent-directories-while-creating-a-new-file
 # Note: `for param do` is short form of `for param in "$@"; do`
+
+ni() {
+  for param do
+    dir="${param%/*}"
+    file="${param##*/}"
+    mkdir -p -- "$dir" &&
+    if [ -n "$file" ]; then touch -- "$param"; fi
+  done
+}
+# ni /path/dir/ /path/dir_b/file
+# will create directory (endswith '/') or files with intermediate directories
